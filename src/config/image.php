91e5918938af149bf596618bfeac26456e072bad
ognestraz <ognestraz@gmail.com>
2016-01-06 16:18:38 +0300

    Add sample config

---
+++ src/config/image.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'variants' => [
+        'original' => [],
+        'icon' => [
+            ['fit' => ['width' => 100, 'height' => 100]]
+        ],
+        'large' => [
+            ['fit' => ['width' => 1280, 'height' => 720]]
+        ],
+        'preview' => [
+            ['fit' => ['width' => 300, 'height' => 200]]
+        ]        
+    ]
+];
